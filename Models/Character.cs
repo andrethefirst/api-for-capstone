8c9af3a92d15c5b2f7c7c58b04d561af43ba4acc
Andre Velasquez <andrethefirst21@gmail.com>
2022-02-27 11:54:16 -0800

    add character model

---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+namespace SuperHero.Models
+{
+  public class Character
+  {
+    public int CharacterId { get; set; }
+    public string Name { get; set; }
+    public string SecretID { get; set; }
+    public int Intelligence { get; set;}
+    public int Speed { get; set; }
+    public int Power { get; set; }
+    public int Combat { get; set; }
+    public int Strength { get; set; }
+    public int Durability { get; set;}
+  }
+}

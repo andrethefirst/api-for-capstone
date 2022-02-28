da819f880772f749531b9c90f1ea84a714da98b6
Andre Velasquez <andrethefirst21@gmail.com>
2022-02-27 16:08:57 -0800

    finish api

---
+++ Migrations/20220228000628_Characters.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apiforcapstone.Migrations
+{
+    public partial class Characters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "CharacterId", "Combat", "Durability", "Intelligence", "Name", "Power", "SecretID", "Speed", "Strength" },
+                values: new object[,]
+                {
+                    { 1, 30, 85, 90, "Raven", 92, "Raven", 62, 50 },
+                    { 2, 50, 70, 55, "Human Torch", 81, "Johnny Storm", 87, 70 },
+                    { 3, 50, 40, 96, "Dr Strange", 99, "Stephen Strange", 76, 80 },
+                    { 4, 89, 87, 87, "Spider Man 2099", 45, "Miguel O'Hara", 77, 88 },
+                    { 5, 95, 40, 50, "Green Arrow", 20, "Oliver Queen", 48, 60 },
+                    { 6, 68, 66, 78, "Zatanna", 89, "Zatanna Zatara", 50, 58 },
+                    { 7, 84, 60, 80, "Capt. of Heart Pirates", 93, "Trafalgar Law", 35, 70 },
+                    { 8, 10, 35, 95, "Yami Yugi", 90, "Yami Yugi", 10, 40 },
+                    { 9, 85, 95, 80, "Darth Vader", 94, "Anakin Skywalker", 50, 90 },
+                    { 10, 84, 70, 60, "The Last Air Bender", 87, "Aang", 89, 60 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 10);
+        }
+    }
+}
--- Models/SuperHeroContext.cs
+++ Models/SuperHeroContext.cs
@@ -11,5 +11,22 @@ namespace SuperHero.Models
     }
 
     public DbSet<Character> Characters { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Character>()
+        .HasData(
+          new Character {CharacterId = 1, Name = "Raven" , SecretID = "Raven" , Intelligence = 90 , Speed = 62 , Power = 92 , Combat = 30 , Strength = 50 , Durability = 85 },
+          new Character {CharacterId = 2, Name = "Human Torch" , SecretID = "Johnny Storm" , Intelligence = 55 , Speed = 87 , Power = 81 , Combat = 50, Strength = 70, Durability = 70 },
+          new Character {CharacterId = 3, Name = "Dr Strange" , SecretID = "Stephen Strange" , Intelligence = 96, Speed = 76 , Power = 99 , Combat = 50 , Strength = 80 , Durability = 40},
+          new Character {CharacterId = 4, Name = "Spider Man 2099" , SecretID = "Miguel O'Hara" , Intelligence = 87 , Speed = 77, Power = 45 , Combat = 89 , Strength = 88 , Durability = 87 },
+          new Character {CharacterId = 5, Name = "Green Arrow" , SecretID = "Oliver Queen" , Intelligence = 50 , Speed = 48, Power = 20, Combat = 95 , Strength = 60 , Durability = 40 },
+          new Character {CharacterId = 6, Name = "Zatanna" , SecretID = "Zatanna Zatara" , Intelligence = 78 , Speed = 50 , Power = 89 , Combat = 68 , Strength = 58 , Durability = 66 },
+          new Character {CharacterId = 7, Name = "Capt. of Heart Pirates" , SecretID = "Trafalgar Law" , Intelligence = 80, Speed = 35 , Power = 93 , Combat = 84 , Strength = 70 , Durability = 60 },
+          new Character {CharacterId = 8, Name = "Yami Yugi" , SecretID = "Yami Yugi" , Intelligence = 95 , Speed = 10 , Power = 90 , Combat = 10 , Strength = 40 , Durability = 35 },
+          new Character {CharacterId = 9, Name = "Darth Vader" , SecretID = "Anakin Skywalker" , Intelligence = 80 , Speed = 50 , Power = 94 , Combat = 85 , Strength = 90 , Durability = 95 },
+          new Character {CharacterId = 10, Name = "The Last Air Bender" , SecretID = "Aang" , Intelligence = 60 , Speed = 89 , Power = 87 , Combat = 84 , Strength = 60 , Durability = 70 }
+        );
+    }
   }
 }

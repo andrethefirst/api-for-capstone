ea8109c38b94cf078c841e3fc0f78aaac797bd31
Andre Velasquez <andrethefirst21@gmail.com>
2022-03-11 21:19:06 -0800

    add characters and update

---
+++ Migrations/20220312051822_Characterss.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apiforcapstone.Migrations
+{
+    public partial class Characterss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "CharacterId", "Combat", "Durability", "Intelligence", "Name", "Power", "SecretID", "Speed", "Strength" },
+                values: new object[,]
+                {
+                    { 11, 86, 90, 90, "Spawn", 92, "Albert Simmons", 80, 76 },
+                    { 28, 84, 60, 96, "Spider-Man", 87, "Peter Parker", 89, 83 },
+                    { 27, 87, 77, 80, "Darth Maul", 87, "Maul", 80, 78 },
+                    { 26, 70, 86, 80, "Dani Moonstar", 89, "Dani Moonstar", 80, 80 },
+                    { 25, 78, 74, 78, "Kingpin", 80, "Wilson Fisk", 40, 84 },
+                    { 24, 70, 67, 86, "Psylocke", 80, "Betsy Braddock", 68, 79 },
+                    { 23, 0, 99, 0, "console.log", 99, "Microsoft Documentation", 0, 99 },
+                    { 22, 70, 91, 80, "Pain", 92, "Nagato Uzumaki", 60, 80 },
+                    { 21, 97, 94, 70, "Super Man", 95, "Clark Kent", 90, 98 },
+                    { 20, 90, 99, 40, "Wolverine", 80, "James Howlett", 70, 90 },
+                    { 19, 100, 100, 100, "The Enlightened One", 100, "Andre Velasquez", 100, 100 },
+                    { 18, 80, 80, 80, "Enlightened Apprentice", 80, "Shaun Kent", 80, 80 },
+                    { 17, 35, 35, 35, "Unenlightened", 35, "Amerik Velasquez", 35, 35 },
+                    { 16, 70, 70, 70, "Master Chief", 70, "John 117", 70, 70 },
+                    { 15, 60, 89, 94, "Dr.Doom", 93, "Victor Von Doom", 30, 70 },
+                    { 14, 30, 60, 99, "Professor X", 94, "Charles Xavier", 1, 86 },
+                    { 13, 60, 79, 50, "Green Lantern", 93, "Hal Jordan", 70, 60 },
+                    { 12, 83, 99, 30, "Deadpool", 25, "Wade Wilson", 65, 80 },
+                    { 29, 94, 95, 50, "Darkseid", 90, "Uxas", 70, 89 },
+                    { 30, 99, 99, 99, "Exodia Kanye", 99, "Kanye West", 99, 99 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "CharacterId",
+                keyValue: 30);
+        }
+    }
+}
--- Models/SuperHeroContext.cs
+++ Models/SuperHeroContext.cs
@@ -25,7 +25,27 @@ namespace SuperHero.Models
           new Character {CharacterId = 7, Name = "Capt. of Heart Pirates" , SecretID = "Trafalgar Law" , Intelligence = 80, Speed = 35 , Power = 93 , Combat = 84 , Strength = 70 , Durability = 60 },
           new Character {CharacterId = 8, Name = "Yami Yugi" , SecretID = "Yami Yugi" , Intelligence = 95 , Speed = 10 , Power = 90 , Combat = 10 , Strength = 40 , Durability = 35 },
           new Character {CharacterId = 9, Name = "Darth Vader" , SecretID = "Anakin Skywalker" , Intelligence = 80 , Speed = 50 , Power = 94 , Combat = 85 , Strength = 90 , Durability = 95 },
-          new Character {CharacterId = 10, Name = "The Last Air Bender" , SecretID = "Aang" , Intelligence = 60 , Speed = 89 , Power = 87 , Combat = 84 , Strength = 60 , Durability = 70 }
+          new Character {CharacterId = 10, Name = "The Last Air Bender" , SecretID = "Aang" , Intelligence = 60 , Speed = 89 , Power = 87 , Combat = 84 , Strength = 60 , Durability = 70 },
+          new Character {CharacterId = 11, Name = "Spawn" , SecretID = "Albert Simmons" , Intelligence = 90 , Speed = 80 , Power = 92 , Combat = 86 , Strength = 76 , Durability = 90 },
+          new Character {CharacterId = 12, Name = "Deadpool" , SecretID = "Wade Wilson" , Intelligence = 30 , Speed = 65 , Power = 25 , Combat = 83, Strength = 80, Durability = 99 },
+          new Character {CharacterId = 13, Name = "Green Lantern" , SecretID = "Hal Jordan" , Intelligence = 50, Speed = 70 , Power = 93 , Combat = 60 , Strength = 60 , Durability = 79},
+          new Character {CharacterId = 14, Name = "Professor X" , SecretID = "Charles Xavier" , Intelligence = 99 , Speed = 1, Power = 94 , Combat = 30 , Strength = 86 , Durability = 60 },
+          new Character {CharacterId = 15, Name = "Dr.Doom" , SecretID = "Victor Von Doom" , Intelligence = 94 , Speed = 30, Power = 93, Combat = 60 , Strength = 70 , Durability = 89 },
+          new Character {CharacterId = 16, Name = "Master Chief" , SecretID = "John 117" , Intelligence = 70 , Speed = 70 , Power = 70 , Combat = 70 , Strength = 70 , Durability = 70 },
+          new Character {CharacterId = 17, Name = "Unenlightened" , SecretID = "Amerik Velasquez" , Intelligence = 35, Speed = 35 , Power = 35 , Combat = 35 , Strength = 35 , Durability = 35 },
+          new Character {CharacterId = 18, Name = "Enlightened Apprentice" , SecretID = "Shaun Kent" , Intelligence = 80 , Speed = 80 , Power = 80 , Combat = 80 , Strength = 80 , Durability = 80 },
+          new Character {CharacterId = 19, Name = "The Enlightened One" , SecretID = "Andre Velasquez" , Intelligence = 100 , Speed = 100 , Power = 100 , Combat = 100 , Strength = 100 , Durability = 100 },
+          new Character {CharacterId = 20, Name = "Wolverine" , SecretID = "James Howlett" , Intelligence = 40 , Speed = 70 , Power = 80 , Combat = 90 , Strength = 90 , Durability = 99 },
+          new Character {CharacterId = 21, Name = "Super Man" , SecretID = "Clark Kent" , Intelligence = 70 , Speed = 90 , Power = 95 , Combat = 97 , Strength = 98 , Durability = 94 },
+          new Character {CharacterId = 22, Name = "Pain" , SecretID = "Nagato Uzumaki" , Intelligence = 80 , Speed = 60 , Power = 92 , Combat = 70, Strength = 80, Durability = 91 },
+          new Character {CharacterId = 23, Name = "console.log" , SecretID = "Microsoft Documentation" , Intelligence = 0, Speed = 0 , Power = 99 , Combat = 0 , Strength = 99 , Durability = 99},
+          new Character {CharacterId = 24, Name = "Psylocke" , SecretID = "Betsy Braddock" , Intelligence = 86 , Speed = 68, Power = 80 , Combat = 70 , Strength = 79 , Durability = 67 },
+          new Character {CharacterId = 25, Name = "Kingpin" , SecretID = "Wilson Fisk" , Intelligence = 78 , Speed = 40, Power = 80, Combat = 78 , Strength = 84 , Durability = 74 },
+          new Character {CharacterId = 26, Name = "Dani Moonstar" , SecretID = "Dani Moonstar" , Intelligence = 80 , Speed = 80 , Power = 89 , Combat = 70 , Strength = 80 , Durability = 86 },
+          new Character {CharacterId = 27, Name = "Darth Maul" , SecretID = "Maul" , Intelligence = 80, Speed = 80 , Power = 87 , Combat = 87 , Strength = 78 , Durability = 77 },
+          new Character {CharacterId = 28, Name = "Spider-Man" , SecretID = "Peter Parker" , Intelligence = 96 , Speed = 89 , Power = 87 , Combat = 84 , Strength = 83 , Durability = 60 },
+          new Character {CharacterId = 29, Name = "Darkseid" , SecretID = "Uxas" , Intelligence = 50 , Speed = 70 , Power = 90 , Combat = 94 , Strength = 89 , Durability = 95 },
+          new Character {CharacterId = 30, Name = "Exodia Kanye" , SecretID = "Kanye West" , Intelligence = 99 , Speed = 99 , Power = 99 , Combat = 99 , Strength = 99 , Durability = 99 }
         );
     }
   }
